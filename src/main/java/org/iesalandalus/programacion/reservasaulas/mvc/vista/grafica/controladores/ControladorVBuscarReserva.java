8bb79429428b827b31428022676c1b5aac7c69e6
profi <profi@DESKTOP-695H3QE>
2022-05-12 13:24:36 +0200

    Aplicación terminada, quedan los estilos

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/grafica/controladores/ControladorVBuscarReserva.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/grafica/controladores/ControladorVBuscarReserva.java
@@ -10,6 +10,7 @@ import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Permanenci
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.PermanenciaPorTramo;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Reserva;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Tramo;
+import org.iesalandalus.programacion.reservasaulas.mvc.vista.grafica.utilidades.Dialogos;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -29,7 +30,6 @@ import javafx.scene.control.ToggleGroup;
 public class ControladorVBuscarReserva {
 	ControladorVPrincipal cVPrincipal=null;
 	private IControlador controladorMVC;
-	private ToggleGroup grupo;
 	
 	public void setControladorVPrincipal(ControladorVPrincipal controladorVPrincipal) {
 		cVPrincipal=controladorVPrincipal;
@@ -53,69 +53,26 @@ public class ControladorVBuscarReserva {
 		colAulaReservas.setCellValueFactory(reserva -> new SimpleStringProperty(reserva.getValue().getAula().getNombre()));
 		colPermReservas.setCellValueFactory(reserva -> new SimpleStringProperty(reserva.getValue().getPermanencia().toString()));
 		colPuntosReservas.setCellValueFactory(reserva -> new SimpleStringProperty(String.valueOf(reserva.getValue().getPuntos())));
-		
-		grupo = new ToggleGroup();
-		rbPorHoras.setToggleGroup(grupo);
-		rbPorTramo.setToggleGroup(grupo);
-		inputHoras.setVisibleRowCount(5);
-		inputHoras.setItems(FXCollections.observableArrayList("08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00"));
-		inputTramo.setItems(FXCollections.observableArrayList("Mañana","Tarde"));
-		inputFecha.setValue(LocalDate.now().plusMonths(1));
-		
 	}
-	
-	private void rbSeleccionado() {
-		  RadioButton seleccionado = (RadioButton)grupo.getSelectedToggle();
-		  if (seleccionado==rbPorHoras) {
-			  inputHoras.setDisable(false);
-			  inputTramo.setDisable(true);
-		  }
-		  else if (seleccionado==rbPorTramo) {
-			  inputHoras.setDisable(true);
-			  inputTramo.setDisable(false);
-		  }
-	}
-	
-	public 
 
-
-
-    @FXML
-    void acPorHoras(ActionEvent event) {
-    	rbSeleccionado();
-    }
-
-    @FXML
-    void acPorTramo(ActionEvent event) {
-    	rbSeleccionado();
-    }
     
     @FXML
     void acBuscarReserva(ActionEvent event) {
-    	lbError.setText("");
-    	reservas.clear();
-    	RadioButton seleccionado = (RadioButton)grupo.getSelectedToggle();
-    	Permanencia permanencia=null;
-    	Tramo tramo=null;
     	List<Reserva> coleccionReservas=null;
-    	if(seleccionado==rbPorHoras) {
-    		permanencia=new PermanenciaPorHora(inputFecha.getValue(),LocalTime.parse(inputHoras.getSelectionModel().getSelectedItem()));
-    	}
-    	else if(seleccionado==rbPorTramo) {
-    		if (inputTramo.getSelectionModel().getSelectedItem()=="Mañana") {
-    			tramo=Tramo.MANANA;
-    		}
-    		else if(inputTramo.getSelectionModel().getSelectedItem()=="Tarde") {
-    			tramo=Tramo.TARDE;
-    		}
-    		permanencia=new PermanenciaPorTramo(inputFecha.getValue(),tramo);
-    	}
-    	coleccionReservas=controladorMVC.getReservasPermanencia(permanencia);
-    	if (coleccionReservas==null) {
-    		lbError.setText("No existe ningún profesor con ese correo");
-    	}
-    	else {
-    		reservas.setAll(coleccionReservas);
+    	try {
+    		lbError.setText("");
+        	reservas.clear();
+        	Tramo tramo=Tramo.MANANA;
+        	Permanencia permanencia=new PermanenciaPorTramo(inputFecha.getValue(),tramo);
+        	coleccionReservas=controladorMVC.getReservasPermanencia(permanencia);
+        	if (coleccionReservas==null) {
+        		lbError.setText("No existe ninguna reserva para ese dia");
+        	}
+        	else {
+        		reservas.setAll(coleccionReservas);
+        	}
+    	}catch(Exception e) {
+    		Dialogos.mostrarDialogoError("Error", e.getMessage());
     	}
     }
     
@@ -124,18 +81,6 @@ public class ControladorVBuscarReserva {
     
     @FXML
     private DatePicker inputFecha;
-
-    @FXML
-    private ComboBox<String> inputHoras;
-
-    @FXML
-    private ComboBox<String> inputTramo;
-    
-    @FXML
-    private RadioButton rbPorHoras;
-
-    @FXML
-    private RadioButton rbPorTramo;
     
     @FXML
     private Button btBuscarReserva;
@@ -148,16 +93,5 @@ public class ControladorVBuscarReserva {
 
     }
 
-    @FXML
-    void acHora(ActionEvent event) {
-
-    }
-
-
-
-    @FXML
-    void acTramo(ActionEvent event) {
-
-    }
 
 }
